1cd158aecb1fdc395b42aadb78e68d07f7148399
shashank rao <shashankraos.xworkz@gmail.com>
2022-03-28 17:06:24 +0530

    javascript

--- javascript/function.js
+++ javascript/function.js
@@ -3,6 +3,8 @@ return var1+var2;
 }
 let r=add(20,20);
 console.log(r);
+
 //arrow function
-let res=(varl1,varl2)=>varl1+varl2;
+let res=(varl1,varl2)=>{
+   return varl1+varl2;}
 console.log(res(10,50));
---
+++ typescript/tsc_basics.js
@@ -0,0 +1,44 @@
+function rao(){
+    var a='i am a';
+    for(var i=0;i<5;i++){
+        let a='im inside for loop';
+    }
+    console.log(a);
+}
+rao();
+
+function funrao(){
+    if(true)
+    {
+        const t=123;
+        console.log(t);
+    }
+    
+}
+funrao();
+
+const myobj={};
+myobj['name']='shashank';
+console.log(myobj);
+
+let msg=
+`
+hiii
+every
+one my
+name 
+is 
+shashank
+`;
+console.log(msg);
+
+let rao1='coding';
+let msg1=
+`
+${rao1}
+is 
+very 
+interesting 
+job
+`;
+console.log(msg1);
