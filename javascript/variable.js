ef353e9d15edf496da4e49f0d0b40cbcd8b9f929
shashank rao <shashankraos.xworkz@gmail.com>
2022-03-28 09:05:39 +0530

    variables fun

---
+++ javascript/variable.js
@@ -0,0 +1,11 @@
+let fname='shashank';
+let lname="rao";
+
+console.log(fname+lname);
+console(typeof fname);
+
+var age=24;
+var address='shimoga';
+
+console.log(age+address);
+console(typeof age);
