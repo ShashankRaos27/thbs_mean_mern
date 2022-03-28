1db2db1027d6162a3757d4473ba46f62daaf0f1b
shashank rao <shashankraos.xworkz@gmail.com>
2022-03-28 12:40:47 +0530

    arrow fun

---
+++ javascript/equality.js
@@ -0,0 +1,9 @@
+let var1='shashank';
+let var2=24;
+if(var1===var2)
+{
+    console.log('they are equal');
+}
+else{
+    console.log('they are not equal');
+}
--- javascript/function.js
+++ javascript/function.js
@@ -1,4 +1,8 @@
 function add(var1,var2){
 return var1+var2;
 }
-add(20,20);
+let r=add(20,20);
+console.log(r);
+//arrow function
+let res=(varl1,varl2)=>varl1+varl2;
+console.log(res(10,50));
